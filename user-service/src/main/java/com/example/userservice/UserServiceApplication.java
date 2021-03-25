c434dd6540df2048167954c195402b430d5cea64
Qassim Ali <qmaa96@gmail.com>
2021-03-25 15:17:36 +0300

    Microservices

---
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class UserServiceApplication {
+// 32:20 Microservices using SpringBoot | Full Example
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+}
